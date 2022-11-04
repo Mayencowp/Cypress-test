cdcf9c9e8b5ba46d6f87c72d7a861e6d823730ab
Mayenco <jlwateruma@gmail.com>
2022-11-04 18:43:53 +0100

    ejercicio login y shifting content

---
+++ cypress/integration/shifting.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it ('Sifting Content can be opened', () => {
+
+cy.visit("https://the-internet.herokuapp.com/")
+cy.get(':nth-child(39) > a').click()
+cy.get('[href="/shifting_content/menu"]') .click ()
+cy.contains ('Home')
+cy.contains ('About')
+cy.contains ('Contact Us')
+cy.contains ('Portfolio')
+cy.contains ('Gallery')
+
+
+})
